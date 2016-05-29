00122a0669fc7a3d8c276a51542ad95e158479a5
Jan Söderholm <jan.soderholm@fgirot.se>
2016-05-29 19:20:25 +0200

    First functionality

--- UnitConverter/Program.cs
+++ UnitConverter/Program.cs
@@ -10,6 +10,15 @@ namespace UnitConverter
     {
         static void Main(string[] args)
         {
+            UnitConverter feetToInches = new UnitConverter(12);
+            UnitConverter milesToFeet = new UnitConverter(5280);
+
+            Console.WriteLine(feetToInches.Convert(30));
+            Console.WriteLine(feetToInches.Convert(100));
+            Console.WriteLine(feetToInches.Convert
+                            (milesToFeet.Convert(1)));
+            Console.Write("Press any key to exit...");
+            Console.ReadKey();
         }
     }
 }
---
+++ UnitConverter/UnitConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitConverter
+{
+    public class UnitConverter
+    {
+        int ratio;
+
+        public UnitConverter (int unitRatio)
+        {
+            ratio = unitRatio;
+        }
+
+        public int Convert (int unit)
+        {
+            return unit * ratio;
+        }
+    }
+}
